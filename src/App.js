44ec5755ef3b52a9cde7fd4b472154bfe464d92d
Dominowa <babicyura@gmail.com>
2023-09-26 15:16:51 +0300

    л

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import Profile from './components/Profile';
+import user from './data/user.json';
+
+function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
+
+export default App;
